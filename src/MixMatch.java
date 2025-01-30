81f2325bd465a4a7815ca70a6e601393353e4831
Bryan3458 <bryan.a.martinez01@utrgv.edu>
2025-01-30 21:11:25 +0000

    First Commit

---
+++ src/MixMatch.java
@@ -0,0 +1,28 @@
+public class MixMatch{
+    public static void main(String[] args) {
+        
+        String a = "Bryan";
+        System.out.println(a);
+
+        int birthday = 18;
+        System.out.println(birthday);
+
+        double num1 = 4.333333333;
+        System.out.println(num1);
+
+        boolean word = true;
+        System.out.println(word);
+
+        char test = 'A';
+        System.out.println(test);
+
+        short rise = 7000;
+        System.out.println(rise);
+
+        float today = 7.2f;
+        System.out.println(today);
+
+        long last = 690000000L;
+        System.out.println(last);
+    }
+}
