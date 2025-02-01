608cac8863a6a990b6accb4086b6e24f45f7afd9
Bryan3458 <bryan.a.martinez01@utrgv.edu>
2025-02-01 17:17:52 +0000

    Added brief descriptions of the data types

--- src/MixMatch.java
+++ src/MixMatch.java
@@ -2,27 +2,27 @@ public class MixMatch{
     public static void main(String[] args) {
         
         String a = "Bryan";
-        System.out.println(a);
+        System.out.println(a + " <- A String stores a sequence of characters");
 
         int birthday = 18;
-        System.out.println(birthday);
+        System.out.println(birthday + " <- An int stores whole numbers from -2^31 to 2^31 -1");
 
         double num1 = 4.333333333;
-        System.out.println(num1);
+        System.out.println(num1 + " <- A double stores fraction numbers up to 15 digits");
 
         boolean word = true;
-        System.out.println(word);
+        System.out.println(word + " <- A boolean store either true or false");
 
         char test = 'A';
-        System.out.println(test);
+        System.out.println(test + " <- A char stores a single character from the ASCII value");
 
         short rise = 7000;
-        System.out.println(rise);
+        System.out.println(rise + " <- A short stores whole numbers from -32,768 to 32,767");
 
         float today = 7.2f;
-        System.out.println(today);
+        System.out.println(today + " <- A float stores fraction numbers up to 7 digits");
 
         long last = 690000000L;
-        System.out.println(last);
+        System.out.println(last + " <- A long stores whole numbers from -2^63 to 2^63 -1");
     }
 }
